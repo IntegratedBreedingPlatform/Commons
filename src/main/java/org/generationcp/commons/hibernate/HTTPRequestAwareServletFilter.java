0e36f9bc1cfdc48fbdd28322062f5331dc17a961
leonel@leafnode.io <leonelkovacic@gmail.com>
2022-01-17 19:17:46 -0300

    IBP-5378: changed security policy

--- src/main/java/org/generationcp/commons/hibernate/HTTPRequestAwareServletFilter.java
+++ src/main/java/org/generationcp/commons/hibernate/HTTPRequestAwareServletFilter.java
@@ -34,9 +34,9 @@ import org.slf4j.LoggerFactory;
 public class HTTPRequestAwareServletFilter implements Filter {
 
 	private static final Logger LOG = LoggerFactory.getLogger(HTTPRequestAwareServletFilter.class);
-	public static final String CSP_CONFIG = "default-src 'self' https://surveyhero.com; "
+	public static final String CSP_CONFIG = "default-src 'self'; "
 		+ "img-src 'self' data: https:; "
-		+ "frame-src 'self' blob:; "
+		+ "frame-src 'self' blob: https://surveyhero.com; "
 		+ "connect-src 'self' https:; "
 		+ "object-src 'none'; "
 		+ "script-src 'self' https://embed-cdn.surveyhero.com 'unsafe-inline' 'unsafe-eval'; "
